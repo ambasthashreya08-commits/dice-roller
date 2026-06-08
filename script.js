6507f4e8519b036ce5a742e2256f0e86b73a7558
ambasthashreya08-commits <ambasthashreya.08@gmail.com>
2026-06-09 00:46:51 +0530

    Enhance UI with animated dice rolling

--- script.js
+++ script.js
@@ -1,6 +1,32 @@
 function rollDice() {
-    const number = Math.floor(Math.random() * 6) + 1;
 
-    document.getElementById("result").innerText =
-        "You rolled: " + number;
+    const diceFaces = ["⚀","⚁","⚂","⚃","⚄","⚅"];
+
+    const result = document.getElementById("result");
+
+    result.classList.add("rolling");
+
+    let count = 0;
+
+    const animation = setInterval(() => {
+
+        result.textContent =
+            diceFaces[Math.floor(Math.random() * 6)];
+
+        count++;
+
+        if(count > 15){
+
+            clearInterval(animation);
+
+            const finalNumber =
+                Math.floor(Math.random() * 6);
+
+            result.textContent =
+                diceFaces[finalNumber];
+
+            result.classList.remove("rolling");
+        }
+
+    },100);
 }
